afae55905dce4103c5f74d1629705065e06b39a6
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-11-20 15:27:36 +1300

    Include some of the eye project calculation code from Dream.

---
+++ source/Euclid/Geometry/Eye.cpp
@@ -0,0 +1,85 @@
+//
+//  Geometry/Viewport.cpp
+//  This file is part of the "Euclid" project, and is released under the MIT license.
+//
+//  Created by Samuel Williams on 5/06/11.
+//  Copyright (c) 2011 Samuel Williams. All rights reserved.
+//
+
+#include "Eye.h"
+
+namespace Euclid
+{
+	namespace Geometry
+	{
+		template <typename NumericT>
+		Vector<4, NumericT> Eye<NumericT>::Transformation::convert_from_projection_space_to_object_space(Vector<4, NumericT> coordinate) const
+		{
+			return inverse_view_matrix * (inverse_projection_matrix * coordinate);
+		}
+		
+		template <typename NumericT>
+		Eye<NumericT> Eye<NumericT>::Transformation::convert_from_viewport_space_to_object_space (const AlignedBox<2, NumericT> & viewport, const Vector<2, NumericT> & point) const
+		{
+			// Reverse the viewport transformation
+			Vector<2, NumericT> normalized_point;
+
+			// Calculate position in normal projection box
+			normalized_point[X] = ((point[X] - viewport.origin()[X]) / viewport.size()[X]) * 2.0 - 1.0;
+			normalized_point[Y] = ((point[Y] - viewport.origin()[Y]) / viewport.size()[Y]) * 2.0 - 1.0;
+
+			return convert_from_normalized_space_to_object_space(normalized_point << near);
+		}
+
+		template <typename NumericT>
+		Eye<NumericT> Eye<NumericT>::Transformation::convert_from_normalized_space_to_object_space (Vector<3, NumericT> normalized_point) const
+		{
+			// Reverse the viewport transformation from normalized device coordinates back into object space
+			Eye<NumericT> result;
+
+			result.origin = Vector<3, NumericT>(inverse_view_matrix.at(3, 0), inverse_view_matrix.at(3, 1), inverse_view_matrix.at(3, 2));
+
+			auto p1 = (inverse_view_matrix * (inverse_projection_matrix * (normalized_point << 1.0)));
+
+			normalized_point[Z] *= -1;
+			auto p2 = (inverse_view_matrix * (inverse_projection_matrix * (normalized_point << 1.0)));
+
+			p1 /= p1[W];
+			p2 /= p2[W];
+
+			result.forward = Line<3, NumericT>(p1.reduce(), (p2 - p1).reduce().normalize());
+
+			// Calculate up direction
+			Vector<3, NumericT> eye_up(0.0, 1.0, 0.0);
+			result.up = inverse_view_matrix * eye_up;
+
+			return result;
+		}
+		
+		template <typename NumericT>
+		Matrix<4, 4, NumericT> Eye<NumericT>::look_at(const Vector<3, NumericT> & origin, const Vector<3, NumericT> & direction, const Vector<3, NumericT> & up)
+		{
+			auto s = direction.cross(up);
+			auto u = s.cross(direction);
+
+			Matrix<4, 4, NumericT> m = ZERO;
+			m.set(0, 0, s, 4);
+			m.set(1, 0, u, 4);
+			m.set(2, 0, -direction, 4);
+			m.at(3, 3) = 1;
+
+			auto t = Matrix<4, 4, NumericT>::translating_matrix(-origin);
+
+			return m.transposed_matrix() * t;
+		}
+		
+		template <typename NumericT>
+		Matrix<4, 4, NumericT> Eye<NumericT>::look_at() const
+		{
+			return look_at(origin, forward.direction(), up);
+		}
+		
+		template class Eye<float>;
+		template class Eye<double>;
+	}
+}
---
+++ source/Euclid/Geometry/Eye.h
@@ -0,0 +1,63 @@
+//
+//  Geometry/Viewport.h
+//  This file is part of the "Euclid" project, and is released under the MIT license.
+//
+//  Created by Samuel Williams on 5/06/11.
+//  Copyright (c) 2011 Samuel Williams. All rights reserved.
+//
+
+#ifndef _EUCLID_GEOMETRY_EYE_H
+#define _EUCLID_GEOMETRY_EYE_H
+
+#include "Line.h"
+#include "AlignedBox.h"
+#include "../Numerics/Matrix.h"
+
+namespace Euclid
+{
+	namespace Geometry
+	{
+		template <typename NumericT = RealT>
+		struct Eye {
+			Vector<3, NumericT> origin;
+			Line<3, NumericT> forward;
+			Vector<3, NumericT> up;
+			
+			struct Transformation {
+				Matrix<4, 4, NumericT> inverse_projection_matrix;
+				Matrix<4, 4, NumericT> inverse_view_matrix;
+				
+				NumericT near = -1.0;
+				
+				Vector<4, NumericT> convert_from_projection_space_to_object_space(Vector<4, NumericT> coordinate) const;
+				
+				/// Calculate the object-space coordinates when given the window's viewport and a point in the viewport.
+				Eye convert_from_viewport_space_to_object_space (const AlignedBox<2, NumericT> & viewport, const Vector<2, NumericT> & point) const;
+
+				/// Calculate the object-space coordinates when given a projection-space coordinate on the near plane.
+				Eye convert_from_normalized_space_to_object_space (Vector<3, NumericT> normalized_point) const;
+			};
+			
+			/// Basically an implementation of gluLookAt, but slightly simpler due to the following constraints:
+			/// - direction is already normalized and points from _origin in the direction we are currently looking in
+			/// - up is already normalized
+			static Matrix<4, 4, NumericT> look_at(const Vector<3, NumericT> & origin, const Vector<3, NumericT> & direction, const Vector<3, NumericT> & up);
+			
+			Matrix<4, 4, NumericT> look_at() const;
+		};
+		
+		template <typename NumericT>
+		typename Eye<NumericT>::Transformation eye_transformation(const Matrix<4, 4, NumericT> & projection_matrix, const Matrix<4, 4, NumericT> & view_matrix, NumericT near = -1) {
+			return typename Eye<NumericT>::Transformation {
+				projection_matrix.inverse_matrix(),
+				view_matrix.inverse_matrix(),
+				near
+			};
+		}
+		
+		extern template class Eye<float>;
+		extern template class Eye<double>;
+	}
+}
+
+#endif
