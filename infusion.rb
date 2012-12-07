257ec5ab71a7bd86ce4b425ff3eeed3d23e5e7ec
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-12-07 14:41:39 +1300

    Update to new dependency based build.

--- infusion.rb
+++ infusion.rb
@@ -3,18 +3,23 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-required_version "0.4"
-require 'teapot/build'
+required_version "0.5"
 
-define_package "euclid-0.1.0" do |package|
-	package.depends = ["unit-test"]
-
-	package.build(:all) do |platform, environment|
-		top = Teapot::Build.top(Pathname.new(__FILE__).dirname)
+define_package "euclid" do |package|
+	package.install do |environment|
+		top = Teapot::Build.top(package.path)
 
 		top.add_directory('source')
 		top.add_directory('test')
 		
 		top.execute(:install, environment)
+	end
+	
+	package.depends :system
+	package.depends "Language/C++11"
+	package.depends "Library/unit-test"
+	
+	package.provides "Library/euclid" do
+		append linkflags "-lEuclid"
 	end
 end
