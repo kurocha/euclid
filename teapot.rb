de45083ca354ed6de3cd508a3cc0703677ec1928
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-03-22 04:36:23 +1300

    Update to support teapot 0.7.0

--- teapot.rb
+++ teapot.rb
@@ -6,8 +6,8 @@
 required_version "0.6"
 
 define_target "euclid" do |target|
-	target.install do |environment|
-		install_directory(package.path, 'source', environment)
+	target.build do |environment|
+		build_directory(package.path, 'source', environment)
 	end
 	
 	target.depends :platform
@@ -19,8 +19,8 @@ define_target "euclid" do |target|
 end
 
 define_target "euclid-tests" do |target|
-	target.install do |environment|
-		install_directory(package.path, 'test', environment)
+	target.build do |environment|
+		build_directory(package.path, 'test', environment)
 	end
 	
 	target.depends :platform
