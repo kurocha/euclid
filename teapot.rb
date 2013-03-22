f088d188f4441fff3bf206cdb4261f1b264d16f7
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-03-23 05:42:04 +1300

    Update to support 0.7.0.

--- teapot.rb
+++ teapot.rb
@@ -3,7 +3,7 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-required_version "0.6"
+required_version "0.7.0"
 
 define_target "euclid" do |target|
 	target.build do |environment|
