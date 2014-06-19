7a1418d4184bd5a271befcb39e1e99ddb7b16251
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-06-20 01:38:39 +1200

    Updated travis config to use more general packages.

--- teapot.rb
+++ teapot.rb
@@ -46,20 +46,11 @@ define_target "euclid-tests" do |target|
 	target.provides "Test/Euclid"
 end
 
-define_configuration "euclid" do |configuration|
+define_configuration "travis" do |configuration|
 	configuration[:source] = "https://github.com/dream-framework"
 	
 	configuration.require "platforms"
 	configuration.require "build-files"
-	configuration.require "unit-test"
-end
-
-define_configuration "travis" do |configuration|
-	configuration[:source] = "https://github.com/dream-framework"
-	
-	configuration.require "platform-linux"
-	configuration.require "build-files"
-	configuration.require "variants"
 	
 	configuration.require "unit-test"
 end
